fee75a62d189923136dc645be0a4a28da7db1bac
kyla w <wallk807@hsd.k12.or.us>
2022-05-22 18:05:48 -0700

    adding JS

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
-var paragraph = document.createElement('p');
+var paragraph = document.createElement('P');
 paragraph.textContent = 'The DOM is the Bomb!';
-document.body.appendChild(paragraph)
+document.body.appendChild(paragraph);
+var div = document.createElement('DIV');
+div.style.height = '100vh';
+document.body.appendChild(div);
+div.addEventListener('mousemove', function(event) {
+    console.log(event);
+    var x = event.clientX;
+    var y = event.clientY;
+    div.textContent = x + ', ' +y;
+    div.style.backgroundColor = 'rbg(' + x + ',' + y +', 100)'
+});
